6d723f0d9fc5deb1f82dd730b262162ea4e88c68
brijeshthakkar178 <brijeshthakkar178@gmail.com>
2024-09-08 19:11:04 +0530

    new template

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const createNextIntlPlugin = require('next-intl/plugin')
-
-const withNextIntl = createNextIntlPlugin()
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = withNextIntl(nextConfig)
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
